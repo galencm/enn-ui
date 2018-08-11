35cfa3524fbff4122975cc9175dcecfba917b0a6
Galen Curwen-McAdams <galencm@fastmail.com>
2018-08-11 13:40:05 +0000

    Problem: devices not visible in gui
    Solution: display discovered and previously
    discovered devices. Check for changes on usb
    events. Save and load session to xml so device
    information can persist between program runs.

--- enn_ui/dev_ui.py
+++ enn_ui/dev_ui.py
@@ -7,11 +7,13 @@
 import argparse
 import atexit
 import threading
-
+import time
+import attr
 import redis
+from lxml import etree
 import keli.slurp_gphoto2 as sg
 import pyudev
-import time
+import os
 from ma_cli import data_models
 
 from kivy.app import App
@@ -24,6 +26,32 @@ from kivy.uix.button import Button
 r_ip, r_port = data_models.service_connection()
 binary_r = redis.StrictRedis(host=r_ip, port=r_port)
 redis_conn = redis.StrictRedis(host=r_ip, port=r_port, decode_responses=True)
+
+@attr.s
+class Device(object):
+    connected = attr.ib(default=False)
+    details = attr.ib(default=attr.Factory(dict))
+
+class DeviceItem(BoxLayout):
+    def __init__(self, *args, **kwargs):
+        self.device = Device()
+        super(DeviceItem, self).__init__()
+        self.details_container = BoxLayout(orientation="vertical")
+        self.add_widget(self.details_container)
+
+    def update_details(self):
+        self.details_container.clear_widgets()
+        connected_color = [.5, .5, .5, 1]
+        if self.device.connected:
+            connected_color = [0, 1, 0, 1]
+        self.details_container.add_widget(Button(background_color=connected_color, height=30, size_hint_y=None))
+        for k, v in self.device.details.items():
+            row = BoxLayout(height=30, size_hint_y=None)
+            key = Label(text=str(k))
+            value = Label(text=str(v))
+            row.add_widget(key)
+            row.add_widget(value)
+            self.details_container.add_widget(row)
 
 class DevApp(App):
     def __init__(self, *args, **kwargs):
@@ -39,11 +67,17 @@ class DevApp(App):
         self.db_port = redis_conn.connection_pool.connection_kwargs["port"]
         self.db_host = redis_conn.connection_pool.connection_kwargs["host"]
         self.env_key = "machinic:env:{}:{}".format(self.db_host, self.db_port)
+        self.session_save_path = "~/.config/enn-ui/"
+        self.session_save_filename = "session_{}_{}.xml".format(self.db_host, self.db_port)
+
         super(DevApp, self).__init__()
 
     def build(self):
         root = BoxLayout()
+        self.device_container = BoxLayout()
+        root.add_widget(self.device_container)
         self.update_env_values()
+        self.load_session()
         # classes for device discovery and interaction
         # .discover() is called for discovery
         self.device_classes = {}
@@ -60,6 +94,7 @@ class DevApp(App):
         # end thread when window is closed
         usb_event_thread.daemon = True
         usb_event_thread.start()
+        self.update_devices()
         return root
 
     def usb_events(self):
@@ -69,7 +104,7 @@ class DevApp(App):
         def log_event(action, device):
             print("action: {} device: {}".format(action, device))
             if action in ("add", "remove"):
-                Clock.schedule_once(lambda dt: self.update_devices(), .1)
+                Clock.schedule_once(lambda dt: self.update_devices(), .01)
 
         observer = pyudev.MonitorObserver(monitor, log_event)
         observer.start()
@@ -78,8 +113,28 @@ class DevApp(App):
             time.sleep(0.1)
 
     def update_devices(self):
+        discovered = []
         for name, device_class in self.device_classes.items():
-            print(device_class.discover())
+            discovered.extend(device_class.discover())
+
+        # reset existing device connected status
+        # before rediscovery / nondiscovery
+        for child in self.device_container.children:
+            child.device.connected = False
+            child.update_details()
+
+        for device in discovered:
+            if not device["uid"] in [child.device.details["uid"] for child in self.device_container.children]:
+                device_widget = DeviceItem()
+                device_widget.device.details = device
+                device_widget.device.connected = True
+                device_widget.update_details()
+                self.device_container.add_widget(device_widget)
+            else:
+                for child in self.device_container.children:
+                    if device["uid"]  == child.device.details["uid"]:
+                        child.device.connected = True
+                        child.update_details()
 
     def update_env_values(self):
         env_values = redis_conn.hgetall(self.env_key)
@@ -88,6 +143,41 @@ class DevApp(App):
         msg = message["channel"].replace("__keyspace@0__:","")
         if msg in (self.env_key):
             Clock.schedule_once(lambda dt: self.update_env_values(), .1)
+
+    def load_session(self):
+        expanded_path = os.path.expanduser(self.session_save_path)
+        file = os.path.join(expanded_path, self.session_save_filename)
+        xml = etree.parse(file)
+        for session in xml.xpath('//session'):
+            for device in session.xpath('//device'):
+                device_widget = DeviceItem()
+                device_widget.device.details = device.attrib
+                device_widget.update_details()
+                self.device_container.add_widget(device_widget)
+
+    def save_session(self):
+        expanded_path = os.path.expanduser(self.session_save_path)
+        if not os.path.isdir(expanded_path):
+            print("creating: {}".format(expanded_path))
+            os.mkdir(expanded_path)
+
+        machine = etree.Element("machine")
+        session = etree.Element("session")
+        machine.append(session)
+
+        # store devices so unfound will show up
+        # on restart
+        for device in [child.device for child in self.device_container.children]:
+            dev =  etree.Element("device")
+            for k, v in device.details.items():
+                dev.set(k, v)
+            session.append(dev)
+        machine_root = etree.ElementTree(machine)
+
+        if os.path.isfile(os.path.join(expanded_path, self.session_save_filename)):
+            os.remove(os.path.join(expanded_path, self.session_save_filename))
+
+        machine_root.write(os.path.join(expanded_path, self.session_save_filename), pretty_print=True)
 
     def on_stop(self):
         # stop pubsub thread if window closed with '[x]'
@@ -110,5 +200,5 @@ def main():
         parser.error("--db-host and --db-port values are both required")
 
     app = DevApp(**vars(args))
-    #atexit.register(app.save_session)
+    atexit.register(app.save_session)
     app.run()
