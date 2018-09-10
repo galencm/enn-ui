5f4d943222e03a8f510587daf6d18d74cbd0b38e
Galen Curwen-McAdams <galencm@fastmail.com>
2018-09-10 01:45:15 +0000

    Problem: code has not been formatted or linted
    Solution: run black to format and flake8 to lint.

--- enn_ui/reference.py
+++ enn_ui/reference.py
@@ -10,6 +10,7 @@ import pathlib
 from lxml import etree
 from ma_cli import data_models
 
+
 def populate_db(db_host, db_port, xml_files=None, verbose=False):
     if db_port is None:
         r_ip, r_port = data_models.service_connection()
@@ -18,26 +19,37 @@ def populate_db(db_host, db_port, xml_files=None, verbose=False):
 
     if not xml_files:
         # get path in module
-        xml_files = [pathlib.PurePath(pathlib.Path(__file__).parents[0], "reference.xml")]
+        xml_files = [
+            pathlib.PurePath(pathlib.Path(__file__).parents[0], "reference.xml")
+        ]
 
-    binary_r = redis.StrictRedis(host=r_ip, port=r_port)
+    # binary_r = redis.StrictRedis(host=r_ip, port=r_port)
     redis_conn = redis.StrictRedis(host=r_ip, port=r_port, decode_responses=True)
     device_script_lookup_key = "device:script_lookup"
     script_lookup_key = "scripts:{}"
     for xml_file in xml_files:
         xml = etree.parse(str(xml_file))
 
-        for script in xml.xpath('//script'):
+        for script in xml.xpath("//script"):
             script_name = script.xpath("./@name")[0]
-            for call in script.xpath('//call'):
-                redis_conn.hset(script_lookup_key.format(script_name), call.xpath("./@name")[0], call.xpath("./@template")[0])
+            for call in script.xpath("//call"):
+                redis_conn.hset(
+                    script_lookup_key.format(script_name),
+                    call.xpath("./@name")[0],
+                    call.xpath("./@template")[0],
+                )
 
-        for device in xml.xpath('//device'):
-            redis_conn.hset(device_script_lookup_key, device.xpath("./@name")[0], device.xpath("./@script")[0])
+        for device in xml.xpath("//device"):
+            redis_conn.hset(
+                device_script_lookup_key,
+                device.xpath("./@name")[0],
+                device.xpath("./@script")[0],
+            )
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--db-host",  default="127.0.0.1", help="db host ip")
+    parser.add_argument("--db-host", default="127.0.0.1", help="db host ip")
     parser.add_argument("--db-port", default=None, help="db port")
     parser.add_argument("--xml-file", nargs="+", default=[], help="xml files")
     parser.add_argument("--verbose", action="store_true", help="")
